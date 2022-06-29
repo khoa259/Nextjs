8ea6234b9d137892e96d2a990d42d95f0b93dfeb
Ng Khoa <khoa10688@gmail.com>
2022-06-29 11:06:51 +0700

    feat: getStaticProps with data

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 
-type Props = {}
+type ProductsProps = {
+  products: any[]
+}
 
-const Products  = (props: Props) => {
-  return (
-    <div>Products</div>
-  )
+const Products = ({ products }: ProductsProps) => {
+  if (!products) return null;
+    return (
+        <div>
+            {products.map((item) => (
+                <div key={item.id}>{item.name}</div>
+            ))}
+        </div>
+    );
+}
+
+export const getStaticProps: GetStaticProps<ProductsProps> = async (
+  context: GetStaticPropsContext) => {
+  console.log("Get Static props - server ");
+  const data = await(await fetch(`http://localhost:3001/products`) ).json()
+  if(!data){
+    return{
+      notFound:true
+    }
+  }
+  return {
+    props: {
+      products:data
+    }
+  }
 }
 
 export default Products 
